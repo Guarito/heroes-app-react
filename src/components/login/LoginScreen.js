16edd4da2dbd60408c398700ba5fdd2dc78a1993
Capassoft <capassomiguel@gmail.com>
2021-06-07 18:53:37 -0400

    Agregando funcionalidad History push replace en Login Component

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,9 +1,20 @@
 import React from "react";
+//Observamos en nuestro arbol de componentes (ReactDevTools) que al utilizar el react router dom, se pasan props por defecto a nuestros componentes. De alli, se trabajara en torno a la prop history y sus distintas funciones
+export const LoginScreen = ({ history }) => {
+    const handleLogin = () => {
+        // console.log(history);
+        //history.push('/');
+        history.replace("/");
+    };
 
-export const LoginScreen = () => {
     return (
-        <div>
+        <div className="container mt-5">
             <h1>Login Screen</h1>
+            <hr />
+
+            <button className="btn btn-primary" onClick={handleLogin}>
+                Login
+            </button>
         </div>
     );
 };
