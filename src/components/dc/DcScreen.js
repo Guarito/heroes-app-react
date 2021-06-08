372be7027c13d30969bf0ee135c30af8eae7d6fc
Capassoft <capassomiguel@gmail.com>
2021-06-08 12:30:51 -0400

    Mostrando el listado basico en cada pantalla dependiendo del publisher

--- src/components/dc/DcScreen.js
+++ src/components/dc/DcScreen.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { HeroesList } from "../heroes/HeroesList";
 
 export const DcScreen = () => {
     return (
         <div>
-            <h1>DC Screen</h1>
+            <HeroesList publisher="DC Comics" />
         </div>
     );
 };
--- src/components/marvel/MarvelScreen.js
+++ src/components/marvel/MarvelScreen.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { HeroesList } from "../heroes/HeroesList";
 
 export const MarvelScreen = () => {
     return (
         <div>
-            <h1>Marvel Screen</h1>
+            <HeroesList publisher="Marvel Comics" />
         </div>
     );
 };
