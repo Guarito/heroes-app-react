75ab718046cec760a71f7054730790e3aeabfa2b
Capassoft <capassomiguel@gmail.com>
2021-06-09 11:07:04 -0400

    Estilizado componente HeroScreen y agregada funcionalidad a un boton de Return

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Redirect, useParams } from "react-router";
 import { getHeroesById } from "../../selectors/getHeroById";
 
-export const HeroScreen = () => {
+export const HeroScreen = ({ history }) => {
     //Hacemos uso del hook useParams para extraer el parametro heroeId de la ruta en este caso
     const params = useParams();
     // console.log(params);
@@ -16,12 +16,46 @@ export const HeroScreen = () => {
         return <Redirect to="/" />;
     }
 
+    const handleReturn = () => {
+        //Utilizamos el metodo goBack ubicado dentro de las props que recibe el componente desde react router dom
+        history.goBack();
+    };
+
     const { superhero, publisher, alter_ego, first_appearance, characters } =
         hero;
 
     return (
-        <div>
-            <h1>{superhero}</h1>
+        <div className="row">
+            <div className="col-4">
+                <img
+                    src={`../assets/heroes/${heroId}.jpg`}
+                    alt={superhero}
+                    className="img-thumbnail"
+                />
+            </div>
+            <div className="col-8">
+                <h3>{superhero}</h3>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item">
+                        <b>Alter ego:</b> {alter_ego}
+                    </li>
+                </ul>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item">
+                        <b>Publisher:</b> {publisher}
+                    </li>
+                </ul>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item">
+                        <b>First appearance:</b> {first_appearance}
+                    </li>
+                </ul>
+                <h5>Characters</h5>
+                <p>{characters}</p>
+                <button className="btn btn-outline-info" onClick={handleReturn}>
+                    Return
+                </button>
+            </div>
         </div>
     );
 };
