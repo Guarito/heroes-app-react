5515cf25c357ac0035d2ee9d101a18dff939d0f6
Capassoft <capassomiguel@gmail.com>
2021-06-14 14:47:24 -0400

    Guardando info de user en localStorage desde componente HeroesApp.js

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -1,14 +1,23 @@
-import React, { useReducer } from "react";
+import React, { useEffect, useReducer } from "react";
 import { AuthContext } from "./auth/AuthContext";
 import { authReducer } from "./auth/authReducer";
 import { AppRouter } from "./routers/AppRouter";
 
 const init = () => {
-    return JSON.parse(localStorage.getItem("user")) || { logged: false };
+    const userLocalStorage = JSON.parse(localStorage.getItem("user")) || {
+        logged: false,
+    };
+    console.log(userLocalStorage);
+    return userLocalStorage;
 };
 
 const HeroesApp = () => {
     const [user, dispatch] = useReducer(authReducer, {}, init);
+    // console.log(user);
+    //Implementamos el efecto para guardar en el localStorage la informacion del user cuando se le clickea a Login
+    useEffect(() => {
+        localStorage.setItem("user", JSON.stringify(user));
+    }, [user]);
     return (
         <AuthContext.Provider value={{ user, dispatch }}>
             <AppRouter />
