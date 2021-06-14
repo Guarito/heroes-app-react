29416f1386856ac7cb6f2f1b11af50daf63c9208
Capassoft <capassomiguel@gmail.com>
2021-06-14 10:44:27 -0400

    Creado authReducer y AuthContext

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { AuthContext } from "./auth/AuthContext";
 import { AppRouter } from "./routers/AppRouter";
 
 const HeroesApp = () => {
-    return <AppRouter />;
+    return (
+        <AuthContext.Provider>
+            <AppRouter />
+        </AuthContext.Provider>
+    );
 };
 
 export default HeroesApp;
