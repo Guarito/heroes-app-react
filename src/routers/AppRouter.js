7381bda4b9195eb3aa146e8c0c847c94b5322fd1
Capassoft <capassomiguel@gmail.com>
2021-06-14 18:05:43 -0400

    Agregando useHistory y redireccionando al clickear logout en el Navbar component

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useContext } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { AuthContext } from "../auth/AuthContext";
 
 import { LoginScreen } from "../components/login/LoginScreen";
 import { DashboardRoutes } from "./DashboardRoutes";
