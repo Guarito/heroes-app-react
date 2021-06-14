4e5b5418c749ae73b116fad0f41a65a26e6b7ecd
Capassoft <capassomiguel@gmail.com>
2021-06-14 10:50:44 -0400

    Usando useReducer y combinandolo con el authReducer

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useReducer } from "react";
 import { AuthContext } from "./auth/AuthContext";
+import { authReducer } from "./auth/authReducer";
 import { AppRouter } from "./routers/AppRouter";
 
+const init = () => {
+    return console.log("Init desde el reducer");
+};
+
 const HeroesApp = () => {
+    const [state, dispatch] = useReducer(authReducer, {}, init);
     return (
-        <AuthContext.Provider>
+        <AuthContext.Provider value={{ state, dispatch }}>
             <AppRouter />
         </AuthContext.Provider>
     );
